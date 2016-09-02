7e8f542e7c793dcf8aa045a11e80f3acf69411c5
diamond <05060050@mail.sanlih.com.tw>
2016-09-02 11:53:30 +0800

    add web_user seeder

---
+++ database/seeds/WebUserTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WebUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	factory(App\Models\WebUser::class, 9999)->create();
+    }
+}
