625ab2b7ac2ae8fd84e20e00dd59b6f775bdfbaa
diamond <05060050@mail.sanlih.com.tw>
2016-09-30 17:18:52 +0800

    modify client page

--- public/endpoint/js/reg.js
+++ public/endpoint/js/reg.js
@@ -1,26 +1,30 @@
 'use strict';
+console.log('test');
+var parser = new UAParser();
+console.log(parser.getResult());
 if ('serviceWorker' in navigator) {
-  console.log('Service Worker is supported');
-  navigator.serviceWorker.register('sw.js').then(function() {
-    return navigator.serviceWorker.ready;
-  }).then(function(reg) {
-    console.log('Service Worker is ready :^)', reg);
-    reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
-      console.log('endpoint:', sub.endpoint);
-      console.log("DEVICE_REGISTRATION_ID: ", sub.endpoint.substr(sub.endpoint.lastIndexOf('/') + 1));
-      var endpoint = sub.endpoint.substr(sub.endpoint.lastIndexOf('/') + 1) ;
-      // var url = 'GCMreg.php';
-      var url = 'https://push.setddg.com/reg/'+endpoint;
-      //url = 'http://localhost:8000/reg/'+endpoint;
-      $.getJSON(url, endpoint, function(){
-        console.log('done');
-      });
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('sw.js').then(function() {
+        return navigator.serviceWorker.ready;
+    }).then(function(reg) {
+        console.log('Service Worker is ready :^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+            console.log("DEVICE_REGISTRATION_ID: ", sub.endpoint.substr(sub.endpoint.lastIndexOf('/') + 1));
+            var endpoint = sub.endpoint.substr(sub.endpoint.lastIndexOf('/') + 1);
+            // var url = 'GCMreg.php';
+            var url = 'https://push.setddg.com/reg/' + endpoint;
+            //url = 'http://localhost:8000/reg/'+endpoint;
+            $.getJSON(url, endpoint, function() {
+                console.log('done');
+            });
+        });
+    }).catch(function(error) {
+        console.log('Service Worker error :^(', error);
     });
-  }).catch(function(error) {
-    console.log('Service Worker error :^(', error);
-  });
 
-}else{
-  alert('the serviceWorker not supported ');
+} else {
+    alert('the serviceWorker not supported ');
 }
-
--- public/endpoint/sw.js
+++ public/endpoint/sw.js
@@ -1,80 +1,78 @@
 'use strict';
 
 var GCM_API_ENDPOINT = 'https://push.setddg.com/push/latest';
-importScripts('./js/analytics.js');
-
-self.analytics.trackingId = 'UA-84589149-2';
 
 console.log('Started');
 
 self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  console.log('Installed', event);
+    self.skipWaiting();
+    console.log('Installed', event);
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('Activated', event);
+    console.log('Activated', event);
 });
 
 self.addEventListener('push', function(event) {
-  console.log('Received Push message', event);
-  event.waitUntil(
-    
-    fetch(GCM_API_ENDPOINT).then(function(response) {
-      if (response.status !== 200) {
-        throw new Error('Invalid status code from  API: ' +
-          response.status);
-      }
-      return response.json();
-    }).then(function(data) {
-      console.log('API data: ', data);
+    console.log('Received Push message', event);
+    event.waitUntil(
 
-      const title = data.title || [];
-      const notificationOptions = {
-        body: data.body || [],
-        icon: data.icon || "images/push.png",
-        tag: data.action || "http://www.setddg.com/"
-      };
-      // var message = data.body || [];
-      // var icon = data.icon || "images/push.png";
-      // var click_url = data.action || "http://www.setddg.com/";
-      // var notificationData = {
-      //          url: click_url
-      //     };
-      // event.waitUntil(
-      //   Promise.all([
-      //     self.registration.showNotification(title, notificationOptions), self.analytics.trackEvent('push-received')
-      //   ])
-      // );
-      return self.registration.showNotification(title, notificationOptions);
+        fetch(GCM_API_ENDPOINT).then(function(response) {
+            if (response.status !== 200) {
+                throw new Error('Invalid status code from  API: ' +
+                    response.status);
+            }
+            return response.json();
+        }).then(function(data) {
+            console.log('API data: ', data);
 
-    }).catch(function(err) {
-      console.error('A Problem occured with handling the push msg', err);
-    })
-    
-  );
+            const title = data.title || [];
+            const notificationOptions = {
+                body: data.body || [],
+                icon: data.icon || "images/ddg.png",
+                tag: data.action || "http://www.setddg.com/",
+                requireInteraction: true
+            };
+            // var message = data.body || [];
+            // var icon = data.icon || "images/push.png";
+            // var click_url = data.action || "http://www.setddg.com/";
+            // var notificationData = {
+            //          url: click_url
+            //     };
+            // event.waitUntil(
+            //   Promise.all([
+            //     self.registration.showNotification(title, notificationOptions), self.analytics.trackEvent('push-received')
+            //   ])
+            // );
+            return self.registration.showNotification(title, notificationOptions);
+
+        }).catch(function(err) {
+            console.error('A Problem occured with handling the push msg', err);
+        })
+
+    );
 
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('Notification click: tag', event.notification.tag);
-  event.notification.close();
-  let clickResponsePromise = Promise.resolve();
-  if (event.notification.data && event.notification.data.url) {
-    clickResponsePromise = clients.openWindow(event.notification.data.url);
-  }
-  event.waitUntil(
-    Promise.all([
-      clickResponsePromise,
-      self.analytics.trackEvent('notification-click')
-    ])
-  );
+    console.log('Notification click: tag', event.notification.tag);
+    event.notification.close();
+    let clickResponsePromise = Promise.resolve();
+    if (event.notification.data && event.notification.data.url) {
+        clickResponsePromise = clients.openWindow(event.notification.data.url);
+    }
+    event.waitUntil(
+        Promise.all([
+            clickResponsePromise,
+            // self.analytics.trackEvent('notification-click')
+        ])
+    );
 });
 
 self.addEventListener('notificationclose', function(event) {
-  event.waitUntil(
-    Promise.all([
-      self.analytics.trackEvent('notification-close')
-    ])
-  );
+    event.waitUntil(
+        Promise.all([
+            // self.analytics.trackEvent('notification-close')
+        ])
+    );
 });
